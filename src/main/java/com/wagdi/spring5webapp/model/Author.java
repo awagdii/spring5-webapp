69091ef14f33035cbee63472414663cf83d8be28
awagdii <ahmed.wagdi.moslem@gmail.com>
2019-08-06 23:01:39 -0500

    Add Authors Thymleaf template

--- src/main/java/com/wagdi/spring5webapp/model/Author.java
+++ src/main/java/com/wagdi/spring5webapp/model/Author.java
@@ -65,6 +65,14 @@ public class Author {
         return id == author.id;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(id);
